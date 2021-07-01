97d9e6d6ad027141a20dc2e907cdced150e0fbbd
Nguyen Dang Toan <nguyendangtritoan2305@gmail.com>
2021-07-01 23:11:02 +0300

    add body

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json())
 
-var aTem = []
+var temperature = {}
 
-app.get("/", (req, res) => {res.send("Welcome to the Users API! ",aTem[aTem.length-1])});
+app.get("/", (req, res) => {res.send(temperature)});
 
 app.post("/post", (req, res) => {
     
     res.send("Send post request success API!")
-    let temperature = req.body.temperature
-    aTem.push(temperature)
-    console.log("Data: ",req.body.temperature)
+    temperature = req.body
+    console.log("Data: ",req.body)
 });
 
 app.listen(process.env.PORT || PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
