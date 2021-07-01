78d9c68b657880caf63030878e6b4940a1a99f59
Nguyen Dang Toan <nguyendangtritoan2305@gmail.com>
2021-07-01 22:52:02 +0300

    fifth

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => res.send("Welcome to the Users API!"));
+
+var aTem = []
+
+app.get("/", (req, res) => {res.send("Welcome to the Users API! ",aTem[aTem.length-1])});
 
 app.post("/post", (req, res) => {
     
     res.send("Send post request success API!")
+    let temperature = req.body.temperature
+    aTem.push(temperature)
     console.log("Data: ",req.body.temperature)
 });
 
